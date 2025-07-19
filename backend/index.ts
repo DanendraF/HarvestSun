7b867dd5a5da16ee2c35690c9f8d21e4fcbbdd44
DanendraF <email.lenovoipslim5i@gmail.com>
2025-07-19 23:26:38 +0700

    fix: perbaiki penggunaan cors dan path router di backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,40 +1,49 @@
 import express from 'express';
-import authRouter from './routes/auth';
 import { supabase } from './services/auth';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import authRouter from './routes/auth';
 import tugasRouter from './routes/tugas';
 import gapoktanRouter from './routes/gapoktan';
 import laporanRouter from './routes/laporan';
 import cuacaRouter from './routes/cuaca';
 import lahanRouter from './routes/lahan';
 import panenRouter from './routes/panen';
-
 import reverseGeocodeRouter from './routes/reverseGeocode';
 
 dotenv.config();
 
 const app = express();
 
+console.log("🔍 Registering routes...");
+
 app.use(cors({
-  origin: 'https://harvestsun.vercel.app', // ganti dengan domain Vercel kamu
-  credentials: true
-}));
-app.options('*', cors({
   origin: 'https://harvestsun.vercel.app',
   credentials: true
 }));
+
+// app.options('*', cors({
+//   origin: 'https://harvestsun.vercel.app',
+//   credentials: true
+// }));
 app.use(express.json());
 
 // Route auth
 app.use('/api/auth', authRouter);
-// Route tugas
+
+
 app.use('/api/tugas', tugasRouter);
+
 app.use('/api/gapoktan', gapoktanRouter);
+
 app.use('/api/laporan', laporanRouter);
+
 app.use('/api/lahan', lahanRouter);
+
 app.use('/api/reverse-geocode', reverseGeocodeRouter);
-app.use('/api', cuacaRouter);
+
+app.use('/api/cuaca', cuacaRouter);
+
 app.use('/api/panen', panenRouter);
 
 async function testSupabaseConnection() {
@@ -53,6 +62,3 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Backend running on http://localhost:${PORT}`);
 }); 
-
-//
-//
